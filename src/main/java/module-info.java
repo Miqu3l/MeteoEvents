a5eddf43965091e0855f06ca0a743a5a3ea74cc2
miquel <mrgarriga@outlook.es>
2024-09-25 22:56:52 +0200

    Entorn inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.meteoevents.meteoevents {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+
+    opens org.meteoevents.meteoevents to javafx.fxml;
+    exports org.meteoevents.meteoevents;
+    exports controllers;
+    opens controllers to javafx.fxml;
+}
